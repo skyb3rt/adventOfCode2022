f7a4be609617546b32d5ac8142c304d81befebbc
Thomas Kristoffersen <thomas.kristoffersen@nav.no>
2022-12-12 12:26:22 +0100

    day08

---
+++ day08_test.py
@@ -0,0 +1,26 @@
+# pylint: skip-file
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+import unittest
+from day08 import get_puzzle, part1, part2
+
+PUZZLE_FILE = "input/day08_test.txt"
+DAY = "8"
+# pylint: skip-file
+
+class Tests(unittest.TestCase):
+    puzzle = get_puzzle(PUZZLE_FILE)
+
+    def test_part1(self):
+        puzzle = self.puzzle
+        assert part1(puzzle) == 21
+
+
+    def test_part2(self):
+        puzzle = self.puzzle
+        assert part2(puzzle) == 8
+
+
+if __name__ == '__main__':
+    unittest.main()
